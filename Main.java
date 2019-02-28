cbcc0d95dc55a790655424c982c1ce3279e24f47
tunga <ssanogom@gmail.com>
2019-02-28 23:21:21 +0000

    Ajouter ma Main.java(Bien)

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World. I am your King and Good Luck To Everyone");
+        System.out.println("Seconde Fois d'Afficher du Texte");
+    }
+}
